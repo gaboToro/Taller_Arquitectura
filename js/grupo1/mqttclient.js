4092138abef130f1be2a980fdca8a3bc17e3d216
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-07-31 10:53:11 -0700

    UPDATE MQTTCLIENT GRUPO 1
    Se modificaron los puertos para hivemq y el servidor local, se añadio para que se reciba los mensaje "json" y las funciones para mostrar porcentajes de uso.

--- js/grupo1/mqttclient.js
+++ js/grupo1/mqttclient.js
@@ -2,27 +2,19 @@
 /*####################################### CLIENTE MQTT ###########################################*/
 /*################################################################################################*/
 
-//var wsbroker = "192.168.0.3";  //mqtt websocket enabled broker
-//var wsbroker = "localhost";
 //var wsbroker = "0.tcp.sa.ngrok.io";
-
-//var wsport = 8083 // port for above
-//var wsport = 14792; // port for above
-//var client = new Paho.MQTT.Client(
-	//wsbroker,
-	//Number(wsport),
-	//"myclientid_" + parseInt(Math.random() * 100, 10)
-//);
 var wsbroker = "broker.hivemq.com";
+//var wsbroker = "localhost";
 
-//var wsport = 8083 // port for above
+//var wsport = 14792; // port for above
 var wsport = 1883; // port for above
-
 var client = new Paho.MQTT.Client(
 	wsbroker,
+	//Number(wsport),
 	Number(8000),
 	"myclientid_" + parseInt(Math.random() * 100, 10)
 );
+
 client.onConnectionLost = function (responseObject) {
 	console.log("connection lost: " + responseObject.errorMessage);
 };
@@ -30,36 +22,72 @@ client.onConnectionLost = function (responseObject) {
 /*################################################################################################*/
 /*####################################### LLEGA EL MENSAJE########################################*/
 /*################################################################################################*/
+let prevCPUValue = 0;
+let prevMemoryValue = 0;
+let prevDiskValue = 0;
+
 
 client.onMessageArrived = function (message) {
 	let destination = message.destinationName;
-	if (destination === "probar1234") {
-		let response = JSON.parse(message.payloadString);
-		dataFormat = response;
-		console.log(parseFloat(dataFormat.CPU));
-		console.log(parseFloat(dataFormat.Memoria));
-		console.log(parseFloat(dataFormat.Disco));
-		addData(
-			myChartCPU,
-			parseFloat(dataFormat.CPU),
-		);
-		addData(
-			myChartMemory,
-			parseFloat(dataFormat.Memoria),
-		);
-		addData(
-			myChartDisck,
-			parseFloat(dataFormat.Disco),
-		);
-	}
+	if (destination === "probar_1") {
+        let response = JSON.parse(message.payloadString);
+        dataFormat = response;
+        let dataCPU = dataFormat.CPU;
+        let dataMemoria = dataFormat.Memoria;
+        let dataDisco = dataFormat.Disco;
+
+        // Calcular la diferencia con respecto al valor anterior
+        let diffCPU = dataCPU - prevCPUValue;
+        let diffMemory = dataMemoria - prevMemoryValue;
+        let diffDisk = dataDisco - prevDiskValue;
+
+        // Calcular el porcentaje de cambio
+        let percentageCPU = calculatePercentage(diffCPU, prevCPUValue);
+        let percentageMemory = calculatePercentage(diffMemory, prevMemoryValue);
+        let percentageDisk = calculatePercentage(diffDisk, prevDiskValue);
+
+        // Actualizar los valores en tiempo real en la página
+        document.getElementById("cpuValue").innerText = dataCPU;
+        document.getElementById("memoryValue").innerText = dataMemoria;
+        document.getElementById("diskValue").innerText = dataDisco;
+
+        // Actualizar los porcentajes en la página
+        document.getElementById("cpuPercentage").innerText = percentageCPU + "%";
+        document.getElementById("memoryPercentage").innerText = percentageMemory + "%";
+        document.getElementById("diskPercentage").innerText = percentageDisk + "%";
+
+        // Actualizar los valores anteriores con los nuevos valores
+        prevCPUValue = dataCPU;
+        prevMemoryValue = dataMemoria;
+        prevDiskValue = dataDisco;
+
+        // Cargar datos CPU, Memoria y Almacenamiento en las gráficas
+        addData(myChartCPU, dataCPU);
+        addData2(myChartMemory, dataMemoria);
+        addData3(myChartDisk, dataDisco);
+    }
 };
+
+// Función para calcular el porcentaje de cambio
+function calculatePercentage(diff, prevValue) {
+    if (prevValue === 0) {
+        return "0"; // Si el valor anterior es cero, el porcentaje de cambio es cero
+    }
+
+    let percentage = ((diff / prevValue) * 100).toFixed(2);
+    if (isFinite(percentage)) {
+        return percentage >= 0 ? "+" + percentage : percentage;
+    } else {
+        return "0";
+    }
+}
 
 var options = {
 	timeout: 3,
 	onSuccess: function () {
 		console.log("mqtt connected");
 		// Connection succeeded; subscribe to our topic, you can add multile lines of these
-		client.subscribe("/test_uce_monitor", { qos: 1 });
+		client.subscribe("probar_1", { qos: 1 });
 	},
 	onFailure: function (message) {
 		console.log("Connection failed: " + message.errorMessage);
