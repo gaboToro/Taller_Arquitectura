904f0aa03ebf0987aab108f3ece4a8de801f87c7
Wadri02 <lumbi.2003.adrian@gmail.com>
2023-08-02 12:11:50 -0500

    Update grupo1.html and mqttclient.js
    Se añadió el parámetro para la recepción de kilobits por segundo

--- js/grupo1/mqttclient.js
+++ js/grupo1/mqttclient.js
@@ -25,7 +25,7 @@ client.onConnectionLost = function (responseObject) {
 let prevCPUValue = 0;
 let prevMemoryValue = 0;
 let prevDiskValue = 0;
-let prevGPUValue = 0;
+let prevRecepcionValue = 0;
 
 client.onMessageArrived = function (message) {
 	let destination = message.destinationName;
@@ -35,38 +35,38 @@ client.onMessageArrived = function (message) {
         let dataCPU = dataFormat.CPU;
         let dataMemoria = dataFormat.Memoria;
         let dataDisco = dataFormat.Disco;
-        let dataGPU = dataFormat.GPU;
+        let dataRecepcion = dataFormat.Recepcion;
 
         // Calcular la diferencia con respecto al valor anterior
         let diffCPU = dataCPU - prevCPUValue;
         let diffMemory = dataMemoria - prevMemoryValue;
         let diffDisk = dataDisco - prevDiskValue;
-        let diffGPU = dataGPU - prevGPUValue;
+        let diffRecepcion = dataRecepcion - prevRecepcionValue;
 
         // Calcular el porcentaje de cambio
         let percentageCPU = calculatePercentage(diffCPU, prevCPUValue);
         let percentageMemory = calculatePercentage(diffMemory, prevMemoryValue);
         let percentageDisk = calculatePercentage(diffDisk, prevDiskValue);
-        let percentageGPU = calculatePercentage(diffGPU, prevGPUValue);
+        let percentageRecepcion = calculatePercentage(diffRecepcion, prevRecepcionValue);
 
         // Actualizar los valores en tiempo real en la página
         document.getElementById("cpuValue").innerText = dataCPU;
         document.getElementById("memoryValue").innerText = dataMemoria;
         document.getElementById("diskValue").innerText = dataDisco;
-        document.getElementById("GPUValue").innerText = dataGPU;
+        document.getElementById("RecepcionValue").innerText = dataRecepcion;
 
         // Actualizar los porcentajes en la página
         document.getElementById("cpuPercentage").innerText = percentageCPU + "%";
         document.getElementById("memoryPercentage").innerText = percentageMemory + "%";
         document.getElementById("diskPercentage").innerText = percentageDisk + "%";
-        document.getElementById("GPUPercentage").innerText = percentageGPU + "%";
+        document.getElementById("RecepcionPercentage").innerText = percentageRecepcion + "%";
 
 
         // Actualizar los valores anteriores con los nuevos valores
         prevCPUValue = dataCPU;
         prevMemoryValue = dataMemoria;
         prevDiskValue = dataDisco;
-        prevGPUValue = dataGPU;
+        prevRecepcionValue = dataRecepcion;
 
         // Cargar datos CPU, Memoria y Almacenamiento en las gráficas
         addData(myChartCPU, dataCPU);
