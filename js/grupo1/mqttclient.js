706fdcde55af748c40c14535585ed4395bbc062c
Wadri02 <lumbi.2003.adrian@gmail.com>
2023-08-02 00:40:36 -0500

    Update mqttclient.js
    Se agrego para recibir datos de GPU

--- js/grupo1/mqttclient.js
+++ js/grupo1/mqttclient.js
@@ -25,7 +25,7 @@ client.onConnectionLost = function (responseObject) {
 let prevCPUValue = 0;
 let prevMemoryValue = 0;
 let prevDiskValue = 0;
-
+let prevGPUValue = 0;
 
 client.onMessageArrived = function (message) {
 	let destination = message.destinationName;
@@ -35,31 +35,38 @@ client.onMessageArrived = function (message) {
         let dataCPU = dataFormat.CPU;
         let dataMemoria = dataFormat.Memoria;
         let dataDisco = dataFormat.Disco;
+        let dataGPU = dataFormat.GPU;
 
         // Calcular la diferencia con respecto al valor anterior
         let diffCPU = dataCPU - prevCPUValue;
         let diffMemory = dataMemoria - prevMemoryValue;
         let diffDisk = dataDisco - prevDiskValue;
+        let diffGPU = dataGPU - prevGPUValue;
 
         // Calcular el porcentaje de cambio
         let percentageCPU = calculatePercentage(diffCPU, prevCPUValue);
         let percentageMemory = calculatePercentage(diffMemory, prevMemoryValue);
         let percentageDisk = calculatePercentage(diffDisk, prevDiskValue);
+        let percentageGPU = calculatePercentage(diffGPU, prevGPUValue);
 
         // Actualizar los valores en tiempo real en la página
         document.getElementById("cpuValue").innerText = dataCPU;
         document.getElementById("memoryValue").innerText = dataMemoria;
         document.getElementById("diskValue").innerText = dataDisco;
+        document.getElementById("GPUValue").innerText = dataGPU;
 
         // Actualizar los porcentajes en la página
         document.getElementById("cpuPercentage").innerText = percentageCPU + "%";
         document.getElementById("memoryPercentage").innerText = percentageMemory + "%";
         document.getElementById("diskPercentage").innerText = percentageDisk + "%";
+        document.getElementById("GPUPercentage").innerText = percentageGPU + "%";
+
 
         // Actualizar los valores anteriores con los nuevos valores
         prevCPUValue = dataCPU;
         prevMemoryValue = dataMemoria;
         prevDiskValue = dataDisco;
+        prevGPUValue = dataGPU;
 
         // Cargar datos CPU, Memoria y Almacenamiento en las gráficas
         addData(myChartCPU, dataCPU);
