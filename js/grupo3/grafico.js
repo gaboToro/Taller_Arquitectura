e75c52c93adb72dc276d15684ed184b7c3508922
gaboToro <gabrietoro@yahoo.com>
2023-08-14 10:51:02 -0500

    Changes v2 JP

--- js/grupo3/grafico.js
+++ js/grupo3/grafico.js
@@ -1,28 +1,27 @@
 /*################################################################################################*/
 /*####################################### DESPLIEGUE DEL GRAFICO #################################*/
 /*################################################################################################*/
-/** 
-const ctx20 = document.getElementById("chart-bars").getContext("2d");
-let labels_n = [];
-let data_n = [];
-let data_n2 = [];
-let data_n3 = [];
-let data_n4 = [];
-let myChart = new Chart(ctx20, {
+
+const ctx1 = document.getElementById("myChartCPU").getContext("2d");
+let labels_nCPU = [];
+let data_n1CPU = [];
+let data_n2CPU = [];
+
+let myChartCPU = new Chart(ctx1, {
 	type: "line",
 	data: {
-		labels: labels_n,
+		labels: labels_nCPU,
 		datasets: [
 			{
 				label: "CPU",
-				data: data_n,
+				data: data_n1CPU,
 				backgroundColor: ["rgba(255, 99, 132, 0.2)"],
 				borderColor: ["rgba(255, 99, 132, 1)"],
 				borderWidth: 1,
 			},
 			{
 				label: "Base Line",
-				data: data_n2,
+				data: data_n2CPU,
 				backgroundColor: ["rgba(0, 0, 255, 0.2)"],
 				borderColor: ["rgba(0, 0, 255,1)"],
 				borderWidth: 1,
@@ -33,8 +32,123 @@ let myChart = new Chart(ctx20, {
 		scales: {
 			y: {
 				beginAtZero: true,
+				ticks: {
+				    color: "black",
+				}
 			},
+			x: {
+			    ticks: {
+				    color: "black",
+				}
+		    }
 		},
+		plugins: {
+            legend: {
+                labels: {
+                    color: "black"
+                }
+            }
+        }
+	},
+});
+
+const ctx2 = document.getElementById("myChartMemory").getContext("2d");
+let labels_nMemory = [];
+let data_n1Memory = [];
+let data_n2Memory = [];
+
+let myChartMemory = new Chart(ctx2, {
+	type: "line",
+	data: {
+		labels: labels_nMemory,
+		datasets: [
+			{
+				label: "MEMORIA",
+				data: data_n1Memory,
+				backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+				borderColor: ["rgba(255, 99, 132, 1)"],
+				borderWidth: 1,
+			},
+			{
+				label: "Base Line",
+				data: data_n2Memory,
+				backgroundColor: ["rgba(0, 0, 255, 0.2)"],
+				borderColor: ["rgba(0, 0, 255,1)"],
+				borderWidth: 1,
+			}
+		],
+	},
+	options: {
+		scales: {
+			y: {
+				beginAtZero: true,
+				ticks: {
+				    color: "black",
+				}
+			},
+			 x: {
+                ticks: {
+                    color: "black",
+                }
+             }
+        },
+        plugins: {
+            legend: {
+                labels: {
+                    color: "black"
+                }
+            }
+        }
+	},
+});
+
+const ctx3 = document.getElementById("myChartDisk").getContext("2d");
+let labels_nDisk = [];
+let data_n1Disk = [];
+let data_n2Disk = [];
+
+let myChartDisk = new Chart(ctx3, {
+	type: "line",
+	data: {
+		labels: labels_nDisk,
+		datasets: [
+			{
+				label: "DISCO DURO",
+				data: data_n1Disk,
+				backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+				borderColor: ["rgba(255, 99, 132, 1)"],
+				borderWidth: 1,
+			},
+			{
+				label: "Base Line",
+				data: data_n2Disk,
+				backgroundColor: ["rgba(0, 0, 255, 0.2)"],
+				borderColor: ["rgba(0, 0, 255,1)"],
+				borderWidth: 1,
+			}
+		],
+	},
+	options: {
+		scales: {
+			y: {
+				beginAtZero: true,
+				ticks: {
+				    color: "white",
+				}
+			},
+			x: {
+                ticks: {
+                    color: "white",
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                labels: {
+                    color: "white"
+                }
+            }
+        }
 	},
 });
 
@@ -48,413 +162,4 @@ function addData(chart, dataS) {
 	chart.data.datasets[0].data.push(dataS);
 	chart.data.datasets[1].data.push(40);
 	chart.update();
-}
-
-
-const ctx21=document.getElementById("chart-line").getContext("2d");
-let labels_n_MEMORY = [];
-let data_n_MEMORYRY = [];
-let data_n2_MEMORRY= [];
-let data_n3_MEMORRY= [];
-let data_n4_MEMORRY= [];
-
-
-
-let mychartMemory = new Chart(ctx21, {
-	type: "line",
-	data: {
-		labels: labels_n_MEMORY,
-		datasets: [
-			{
-				label: "RAM",
-				data: data_n_MEMORYRY,
-				backgroundColor: ["rgba(255, 99, 132, 0.2)"],
-				borderColor: ["rgba(255, 99, 132, 1)"],
-				borderWidth: 1,
-			},
-			{
-				label: "Base line",
-				data: data_n2_MEMORRY,
-				backgroundColor: ["rgba(0, 0, 255, 0.2)"],
-				borderColor: ["rgba(0, 0, 255,1)"],
-				borderWidth: 1,
-			}
-		],
-	},
-	options: {
-		scales: {
-			y: {
-				beginAtZero: true,
-			},
-		},
-	},
-});
-
-
-function addData_memory(chart, dataS) {
-	let today = new Date();
-	let date =
-		today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-	// Datos recolectados del sensor
-	chart.data.labels.push(date);
-    chart.data.datasets[0].data.push(dataS);
-    chart.data.datasets[1].data.push(40);
-	chart.update();
-}
-
-
-const ctx22=document.getElementById("chart-line-tasks").getContext("2d");
-let labels_n_disco = [];
-let data_n_disco  = [];
-let data_n2_disco = [];
-let data_n3_disco = [];
-let data_n4_disco = [];
-
-
-
-let myChartDisco = new Chart(ctx22, {
-	type: "line",
-	data: {
-		labels: labels_n_disco,
-		datasets: [
-			{
-				label: "Disco",
-				data: data_n_disco,
-				backgroundColor: ["rgba(255, 99, 132, 0.2)"],
-				borderColor: ["rgba(255, 99, 132, 1)"],
-				borderWidth: 1,
-			},
-			{
-				label: "Base line",
-				data: data_n2_disco,
-				backgroundColor: ["rgba(0, 0, 255, 0.2)"],
-				borderColor: ["rgba(0, 0, 255,1)"],
-				borderWidth: 1,
-			}
-		],
-	},
-	options: {
-		scales: {
-			y: {
-				beginAtZero: true,
-			},
-		},
-	},
-});
-
-function addData_disco(chart, dataS) {
-	let today = new Date();
-	let date =
-		today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-	// Datos recolectados del sensor
-	chart.data.labels.push(date);
-    chart.data.datasets[0].data.push(dataS);
-    chart.data.datasets[1].data.push(40);
-	chart.update();
-}
-
-*/
-/*################################################################################################*/
-/*####################################### DESPLIEGUE DEL GRAFICO #################################*/
-/*################################################################################################*/
-
-
-var ctx = document.getElementById("chart-bars").getContext("2d");
-let labels_n = [];
-let data_n = [];
-let ChartCPU=new Chart(ctx, {
-  type: "line",
-  data: {
-	labels: labels_n,
-	datasets: [{
-	  label: "Sales",
-	  tension: 0.4,
-	  borderWidth: 0,
-	  borderRadius: 4,
-	  borderSkipped: false,
-	  backgroundColor: "rgba(255, 255, 255, .8)",
-	  data: data_n,
-	  maxBarThickness: 6
-	}, ],
-  },
-  options: {
-	responsive: true,
-	maintainAspectRatio: false,
-	plugins: {
-	  legend: {
-		display: false,
-	  }
-	},
-	interaction: {
-	  intersect: false,
-	  mode: 'index',
-	},
-	scales: {
-	  y: {
-		grid: {
-		  drawBorder: false,
-		  display: true,
-		  drawOnChartArea: true,
-		  drawTicks: false,
-		  borderDash: [5, 5],
-		  color: 'rgba(255, 255, 255, .2)'
-		},
-		ticks: {
-		  suggestedMin: 0,
-		  suggestedMax: 500,
-		  beginAtZero: true,
-		  padding: 10,
-		  font: {
-			size: 14,
-			weight: 300,
-			family: "Roboto",
-			style: 'normal',
-			lineHeight: 2
-		  },
-		  color: "#fff"
-		},
-	  },
-	  x: {
-		grid: {
-		  drawBorder: false,
-		  display: true,
-		  drawOnChartArea: true,
-		  drawTicks: false,
-		  borderDash: [5, 5],
-		  color: 'rgba(255, 255, 255, .2)'
-		},
-		ticks: {
-		  display: true,
-		  color: '#f8f9fa',
-		  padding: 10,
-		  font: {
-			size: 14,
-			weight: 300,
-			family: "Roboto",
-			style: 'normal',
-			lineHeight: 2
-		  },
-		}
-	  },
-	},
-  },
-});
-
-function addDataCPU(chart, dataS) {
-
-let today = new Date();
-let date =
-  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-// Datos recolectados del sensor
-chart.data.labels.push(date);
-chart.data.datasets[0].data.push(dataS);
-chart.update();
-}
-
-
-var ctx2 = document.getElementById("chart-line").getContext("2d");
-let labels_n2 = [];
-let data_n2 = [];
-let ChartRAM=new Chart(ctx2, {
-  type: "line",
-  data: {
-	labels: labels_n2,
-	datasets: [{
-	  label: "RAM",
-	  tension: 0,
-	  borderWidth: 0,
-	  pointRadius: 5,
-	  pointBackgroundColor: "rgba(255, 255, 255, .8)",
-	  pointBorderColor: "transparent",
-	  borderColor: "rgba(255, 255, 255, .8)",
-	  borderColor: "rgba(255, 255, 255, .8)",
-	  borderWidth: 4,
-	  backgroundColor: "transparent",
-	  fill: true,
-	  data: data_n2,
-	  maxBarThickness: 6
-
-	}],
-  },
-  options: {
-	responsive: true,
-	maintainAspectRatio: false,
-	plugins: {
-	  legend: {
-		display: false,
-	  }
-	},
-	interaction: {
-	  intersect: false,
-	  mode: 'index',
-	},
-	scales: {
-	  y: {
-		grid: {
-		  drawBorder: false,
-		  display: true,
-		  drawOnChartArea: true,
-		  drawTicks: false,
-		  borderDash: [5, 5],
-		  color: 'rgba(255, 255, 255, .2)'
-		},
-		ticks: {
-		  display: true,
-		  color: '#f8f9fa',
-		  padding: 10,
-		  font: {
-			size: 14,
-			weight: 300,
-			family: "Roboto",
-			style: 'normal',
-			lineHeight: 2
-		  },
-		}
-	  },
-	  x: {
-		grid: {
-		  drawBorder: false,
-		  display: false,
-		  drawOnChartArea: false,
-		  drawTicks: false,
-		  borderDash: [5, 5]
-		},
-		ticks: {
-		  display: true,
-		  color: '#f8f9fa',
-		  padding: 10,
-		  font: {
-			size: 14,
-			weight: 300,
-			family: "Roboto",
-			style: 'normal',
-			lineHeight: 2
-		  },
-		}
-	  },
-	},
-  },
-});
-function addDataRAM(chart, dataS) {
-
-let today = new Date();
-let date =
-today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-// Datos recolectados del sensor
-chart.data.labels.push(date);
-chart.data.datasets[0].data.push(dataS);
-chart.update();
-}
-
-
-var ctx3 = document.getElementById("chart-line-tasks").getContext("2d");
-let labels_n3 = [];
-let data_n3 = [];
-let ChartDISCO=new Chart(ctx3, {
-  type: "line",
-  data: {
-	labels: labels_n3,
-	datasets: [{
-	  label: "Mobile apps",
-	  tension: 0,
-	  borderWidth: 0,
-	  pointRadius: 5,
-	  pointBackgroundColor: "rgba(255, 255, 255, .8)",
-	  pointBorderColor: "transparent",
-	  borderColor: "rgba(255, 255, 255, .8)",
-	  borderWidth: 4,
-	  backgroundColor: "transparent",
-	  fill: true,
-	  data: data_n3,
-	  maxBarThickness: 6
-
-	}],
-  },
-  options: {
-	responsive: true,
-	maintainAspectRatio: false,
-	plugins: {
-	  legend: {
-		display: false,
-	  }
-	},
-	interaction: {
-	  intersect: false,
-	  mode: 'index',
-	},
-	scales: {
-	  y: {
-		grid: {
-		  drawBorder: false,
-		  display: true,
-		  drawOnChartArea: true,
-		  drawTicks: false,
-		  borderDash: [5, 5],
-		  color: 'rgba(255, 255, 255, .2)'
-		},
-		ticks: {
-		  display: true,
-		  padding: 10,
-		  color: '#f8f9fa',
-		  font: {
-			size: 14,
-			weight: 300,
-			family: "Roboto",
-			style: 'normal',
-			lineHeight: 2
-		  },
-		}
-	  },
-	  x: {
-		grid: {
-		  drawBorder: false,
-		  display: false,
-		  drawOnChartArea: false,
-		  drawTicks: false,
-		  borderDash: [5, 5]
-		},
-		ticks: {
-		  display: true,
-		  color: '#f8f9fa',
-		  padding: 10,
-		  font: {
-			size: 14,
-			weight: 300,
-			family: "Roboto",
-			style: 'normal',
-			lineHeight: 2
-		  },
-		}
-	  },
-	},
-  },
-});
-function addDataDISCO(chart, dataS) {
-
-let today = new Date();
-let date =
-  today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-// Datos recolectados del sensor
-chart.data.labels.push(date);
-chart.data.datasets[0].data.push(dataS);
-chart.update();
-}
-
-function updateCPU(valor){
-	const cteRam=document.getElementById("CPU-value");
-	cteRam.textContent=valor ;
-}
-
-function updateRAM(VALOR){
-	const constRam=document.getElementById("RAM-value");
-	constRam.textContent=VALOR
-}
-
-function updateDISCO(VALOR){
-	const constRam=document.getElementById("DISCO-value");
-	constRam.textContent=VALOR
-}
-function updateRED(VALOR){
-	const constRam=document.getElementById("RED-value");
-	constRam.textContent=VALOR
 }
